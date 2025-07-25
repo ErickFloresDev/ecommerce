8757b8175ad70b6a5a7c8cd41a79957267c20025
junniorDeveloper <erick@app.berrylabs.la>
2025-07-25 18:02:35 -0500

    v1.0.2

--- public/js/shop.js
+++ public/js/shop.js
@@ -19,19 +19,18 @@ export async function init() {
   }
 
   const html = products.map(product => `
-    <div class="bg-white flex flex-col justify-between rounded-xl shadow-md border overflow-hidden">
+    <div class="bg-white flex flex-col justify-between rounded-xl border overflow-hidden">
       <a href="/detail" data-id="${product.id}" class="product-link focus:outline-none">
         <div class="relative flex justify-center items-center p-2 md:p-6">
-          <img src="${product.image}" alt="${product.name}" class="w-auto h-32 sm:h-40 md:h-48 object-cover">
-          ${product.price_end < product.price ? '<span class="absolute top-2 right-2 bg-white text-red-500 font-semibold border py-0 md:py-1 px-2 rounded-full  font-tiktok-offert">Oferta</span>' : ''}
+          <img src="${product.image}" alt="${product.name}" class="w-auto h-48 sm:h-40 md:h-48 object-cover">
+          ${product.price_end < product.price ? '<span class="absolute top-2 right-2 bg-white text-red-500 border py-0  px-2 rounded-full  font-tiktok-offert">Oferta</span>' : ''}
         </div>
       </a>
 
         <div class="p-4">
+          <p class="text-gray-500 font-tiktok-offert">${product.category}</p>
+          <h2 class="truncate-text font-tiktok-product mb-2">${product.name}</h2>
           
-          <h2 class="truncate-text font-tiktok-product ">${product.name}</h2>
-          <p class="text-gray-500 font-tiktok-category mt-1 mb-2 truncate-description ">${product.description}</p>
-          <p class="text-gray-500 font-tiktok-category mt-1 mb-2"> <span class="border px-2 rounded-full">${product.category}</span></p>
           <div class="flex justify-between items-center ">
             <div class="flex flex-wrap items-center">
               <p class="font-tiktok-price mr-2">S/ ${product.price_end.toFixed(2)}</p>
@@ -40,15 +39,26 @@ export async function init() {
           </div>
           
           <div class="flex flex-wrap justify-between items-center gap-2 mt-4">
-              <a href="/detail" data-id="${product.id}" class="product-link font-tiktok-category bg-white border  py-2 px-4 rounded-lg flex items-center justify-center w-full">
-                <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-eye mr-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
-                Ver detalle
-              </a>
-              <button class="add-to-cart-btn bg-black font-tiktok-category text-white py-2 px-4 rounded-lg w-full flex items-center justify-center" data-id="${product.id}">
-                <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="#ffffff"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart mr-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17h-11v-14h-2" /><path d="M6 5l14 1l-1 7h-13" /></svg>
-                Agregar
-              </button>
+            <a href="/detail" data-id="${product.id}" class="product-link font-tiktok-category bg-white border py-2 px-4 rounded-lg flex items-center justify-center flex-1 sm:flex-auto">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-eye mr-2">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+              </svg>
+              Ver detalle
+            </a>
+            <button class="add-to-cart-btn bg-black font-tiktok-category text-white py-2 px-4 rounded-lg flex items-center justify-center flex-1 sm:flex-auto" data-id="${product.id}">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart mr-2">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                <path d="M17 17h-11v-14h-2" />
+                <path d="M6 5l14 1l-1 7h-13" />
+              </svg>
+              Agregar
+            </button>
           </div>
+
         </div>
       
     </div>
